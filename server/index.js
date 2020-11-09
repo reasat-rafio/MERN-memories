8129fe6c06bdac7bc8b1e43457b341b0726b4783
reasat-rafio <rafioar96@gmail.com>
2020-11-09 23:25:36 +0600

    connected to mongo

--- server/index.js
+++ server/index.js
@@ -5,6 +5,21 @@ import cors from "cors";
 
 const app = express();
 
-app.use(bodyParse.json({ limit: "30mb", extended: true }));
-app.use(bodyParse.urlencoded({ limit: "30mb", extended: true }));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+const CONNECTION_URL =
+   "mongodb+srv://raf:1234@testcluster1.s4ly9.mongodb.net/memories?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+mongoose
+   .connect(CONNECTION_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+   })
+   .then((res) =>
+      app.listen(PORT, console.log(`Server running on port ${PORT}`))
+   )
+   .catch((err) => console.log(err.message));
