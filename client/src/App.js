12c40ffb11e400e1bfd4a5b956e30fd0d9f6f790
reasat-rafio <rafioar96@gmail.com>
2020-11-10 00:32:52 +0600

    font-end skeleton

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,37 @@
 import "./App.css";
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+import memories from "./images/42509-Memories.jpg";
+import Posts from "./component/Posts/Posts";
+import Form from "./component/Form/Form";
 
 function App() {
-   return <div></div>;
+   return (
+      <Container maxWidth="lg">
+         <AppBar position="static" color="inherit">
+            <Typography varaint="h2" align="center">
+               Memories
+            </Typography>
+            <img src={memories} alt="memories" height="120" />
+         </AppBar>
+         <Grow in>
+            <Container>
+               <Grid
+                  container
+                  justify="space-between"
+                  alignItems="stretch"
+                  spacing={3}
+               >
+                  <Grid item xs={12} sm={7}>
+                     <Posts />
+                  </Grid>
+                  <Grid item xs={12} sm={4}>
+                     <Form />
+                  </Grid>
+               </Grid>
+            </Container>
+         </Grow>
+      </Container>
+   );
 }
 
 export default App;
---
+++ client/src/component/Posts/Posts.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Post from "./Post/Post";
+
+const Posts = () => {
+   return (
+      <>
+         <div>POSTS</div>
+         <Post />
+         <Post />
+      </>
+   );
+};
+
+export default Posts;
